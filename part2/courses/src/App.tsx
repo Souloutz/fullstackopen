182be6f594ff0016a8fa0629151e830d1fc6668b
Howard Kong <howardkong826@gmail.com>
2025-01-23 23:41:09 -0500

    Add total exercises line and modify courses

--- part2/courses/src/App.tsx
+++ part2/courses/src/App.tsx
@@ -2,30 +2,54 @@ import './App.css'
 import Course from './components/Course';
 
 function App() {
-  const course = {
-    id: 1,
-    name: 'Half Stack Application Development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      }, 
-      {
-        name: 'Using Props to Pass Data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a Component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  };
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack Application Development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        }, 
+        {
+          name: 'Using Props to Pass Data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a Component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Reduce',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }];
 
   return (
-    <Course course={course} />  
+    <>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </>
   );
 }
 
--- part2/courses/src/components/Content.tsx
+++ part2/courses/src/components/Content.tsx
@@ -9,6 +9,7 @@ const Content: React.FC<Props> = ({ parts }) => {
     return (
         <>
             {parts.map(part => <Line key={part.id} name={part.name} exercises={part.exercises} />)}
+            <p>Total of {parts.reduce((sum, part) => sum += part.exercises, 0)} exercises</p>
         </>
     );
 };
