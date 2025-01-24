d6510b516f27a32fa756fad3933812d968a6ba8f
Howard Kong <howardkong826@gmail.com>
2025-01-24 09:42:47 -0500

    Add number storing and update keys

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -1,48 +1,41 @@
 import { FormEvent, useState } from 'react';
 import './App.css';
+import Form from './components/Form';
 
 type Person = {
   name: string;
+  number: string;
 }
 
 const App = () => {
   const [persons, setPersons] = useState<Person[]>([
-    { name: 'Howard Kong' }
+    { name: 'Howard Kong', number: '917-517-5963' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault(); // prevent default GET and reloading of page
     
-    const newPerson: Person = {name: newName};
-    const exists = persons.some(person => person.name === newPerson.name);
+    const newPerson: Person = {name: newName, number: newNumber};
+    const exists = persons.some(person => person.name === newPerson.name && person.number === newPerson.number);
     if (exists) {
       alert(`${newName} has already been added to the phonebook!`)
       return
     }
 
-    if (newName.length > 0)
+    if (newName.length > 0 && newNumber.length > 0)
       setPersons([...persons].concat(newPerson));
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div>
-          Name: <input onChange={e => setNewName(e.target.value)}/>
-        </div>
-
-        {/* <div>Debug: {newName}</div> */}
-
-        <div>
-          <button type='submit'>Add</button>
-        </div>
-      </form>
+      <Form handleSubmit={handleSubmit} setNewName={setNewName} setNewNumber={setNewNumber} />
 
       <h2>Numbers</h2>
       {persons.length === 0 ? <p>No numbers saved</p> :
-      persons.map(person => <p key={person.name}>{person.name}</p>)}
+      persons.map(person => <p key={person.number}>{person.name} - {person.number}</p>)}
     </div>
   );
 };
---
+++ part2/phonebook/src/components/Form.tsx
@@ -0,0 +1,28 @@
+type Props = {
+    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+    setNewName: React.Dispatch<React.SetStateAction<string>>;
+    setNewNumber: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Form: React.FC<Props> = ({ handleSubmit, setNewName, setNewNumber }) => {
+    return (
+        <>
+            <form style={{ display: 'flex', flexDirection: 'column', gap: 10 }}onSubmit={(e) => handleSubmit(e)}>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
+                    <div>
+                        Name: <input onChange={e => setNewName(e.target.value)}/>
+                    </div>
+                    <div>
+                        Number: <input onChange={e => setNewNumber(e.target.value)}/>
+                    </div>
+                </div>
+
+                <div>
+                    <button type='submit'>Add</button>
+                </div>
+            </form>
+        </>
+    );
+}
+
+export default Form;
