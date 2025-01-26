2b9d8aeb828e0ac5429aa17734e4ced8a3526dc1
Howard Kong <howardkong826@gmail.com>
2025-01-26 16:43:35 -0500

    Add additional CRUD operations

--- notely-server/index.js
+++ notely-server/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+app.use(express.json()); // used for accessing request body
 
 let notes = [
     {
@@ -23,7 +24,7 @@ let notes = [
         "important": false
     },
     {   
-        "id": "014d",
+        "id": "4",
         "content": "Saved",
         "important": false
     }
@@ -35,6 +36,50 @@ app.get('/', (req, res) => {
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const note = notes.find(note => note.id === id);
+    
+    if (note) {
+        res.json(note);
+    } else {
+        // end responds without sending any data
+        res.status(404).end();
+    }
+});
+
+const generateID = () => {
+    // console.log(...notes.map(note => Number(note.id)));
+    const maxID = notes.length > 0 ? Math.max(...notes.map(note => Number(note.id))) : 0;
+    return String(maxID + 1);
+}
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body;
+
+    if (!body.content) {
+        return res.status(400).json({
+            error: 'Content missing'
+        });
+    }
+
+    const note = {
+        id: generateID(),
+        content: body.content,
+        important: Boolean(body.important) || false,
+    }
+
+    notes = notes.concat(note);
+    res.json(note);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    notes = notes.filter(note => note.id !== id);
+
+    res.status(204).end();
 });
 
 app.listen(port, () => {
