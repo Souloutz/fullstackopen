8c2339a046dad42589ac2473e18e3a1409d023ce
Howard Kong <howardkong826@gmail.com>
2025-01-27 14:46:19 -0500

    Add state and button to toggle displaying of information

--- part2/countries/src/components/CountryResults.tsx
+++ part2/countries/src/components/CountryResults.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Country } from "../services/country_struct"
 import CountryInfo from "./CountryInfo";
 
@@ -6,6 +7,14 @@ type Props = {
 }
 
 const CountryResult: React.FC<Props> =({ countries }) => {
+    const [shown, setShown] = useState<boolean[]>(countries.map(_ => false));
+
+    const handleShow = (index: number): void => {
+        const copy = [...shown];
+        copy[index] = !copy[index];
+        setShown(copy);
+    };
+
     return (
         <div style={{ marginTop: '2em' }}>
             {
@@ -14,7 +23,18 @@ const CountryResult: React.FC<Props> =({ countries }) => {
 
                 countries.length > 10 ? 
                     <p style={{ marginTop: '4em' }}>Total many matches! Please narrow your search.</p> : 
-                    countries.map(filtered => <p key={filtered.name}>{filtered.name}</p>)
+
+                    countries.map((filtered, index) => 
+                        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <p key={filtered.name}>{filtered.name}</p>
+                            
+                            <button style={{ fontSize: '0.8rem', height: 'fit-content' }} onClick={() => handleShow(index)}>
+                                {shown[index] ? 'Hide' : 'Show'}
+                            </button>
+
+                            {shown[index] ? <CountryInfo country={filtered} /> : <></>}
+                        </div>
+                    )
             }
         </div>
     );
