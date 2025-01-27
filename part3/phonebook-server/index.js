b588812ba5ff67eae5c1366ec8d32045268f91c4
Howard Kong <howardkong826@gmail.com>
2025-01-26 19:08:33 -0500

    Implement additional API operations

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -1,21 +1,83 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT | 3001;
 app.use(express.json());
 
-const persons = require('./persons.json');
+let persons = require('./persons.json');
 
 app.get('/', (req, res) => {
     res.send('<h1>PhoneBook Server</h1>');
 });
 
+app.get('/info', (req, res) => {
+    res.send(`
+        <p>PhoneBook has info for ${persons.length} people!</p>
+        <p>${(new Date()).toUTCString()}</p>
+    `);
+});
+
 app.get('/api/persons', (req, res) => {
     res.json(persons);
+});
+
+const generateID = () => {
+    return Math.floor(Math.random() * Number.MAX_VALUE);
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+
+    if (!body) {
+        return res.status(400).json({
+            error: 'Content missing'
+        });
+    }
+
+    const name = body.name;
+    const number = body.number;
+
+    if (!name || !number) {
+        return res.status(400).json({
+            error: 'Content missing'
+        });
+    }
+
+    if (persons.some(person => person.name === name)) {
+        return res.status(400).json({
+            error: 'Duplicate name (must be unique)'
+        });
+    }
+
+    const newPerson = {
+        id: generateID(),
+        name: name,
+        number: number
+    };
+
+    persons = persons.concat(newPerson);
+    res.status(201).json(newPerson);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(person => person.id !== id);
+
+    res.status(204).end();
 });
 
 app.listen(port, () => {
