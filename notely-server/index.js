a05ab54add8b283a5d6295b202a71d2e489d1b7e
Howard Kong <howardkong826@gmail.com>
2025-02-02 17:54:26 -0500

    Update remaining endpoints and add errorhandler middleware

--- notely-server/index.js
+++ notely-server/index.js
@@ -28,64 +28,80 @@ app.use(requestLogger);
 app.use(express.static('dist')); // serve index.html in production build on '/' route
 
 app.get('/api/notes', (req, res) => {
-    Note.find({}).then(notes => {
-        res.json(notes);
-    });
+    Note.find({})
+        .then(notes => {
+            res.json(notes);
+        })
+        .catch(err => next(err));
 });
 
 app.get('/api/notes/:id', (req, res) => {
-    Note.findById(req.params.id).then(note => {
-        if (note === null) 
-            return res.status(404).end();
+    Note.findById(req.params.id)
+        .then(note => {
+            if (note === null) 
+                return res.status(404).end();
 
-        res.json(note);
-    })
+            res.json(note);
+        })
+        .catch(err => next(err));
 });
 
 app.post('/api/notes', (req, res) => {
     const body = req.body;
 
-    if (!body.content) {
-        return res.status(400).json({
-            error: 'Content missing'
-        });
-    }
+    if (!body.content) 
+        return res.status(400).json({ error: 'Content missing' });
 
     const newNote = new Note({
         content: body.content,
         important: Boolean(body.important) || false,
     });
 
-    newNote.save().then(savedNote => {
-        res.json(savedNote);
-    })
+    newNote.save()
+        .then(savedNote => {
+            res.json(savedNote);
+        })
+        .catch(err => next(err));
 });
 
 app.put('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    const note = notes.find(note => note.id === id);
-    note.important = !note.important;
+    const body = req.body
     
-    if (note) {
-        res.json(note);
-    } else {
-        res.status(404).end();
+    const note = {
+        content: body.content,
+        important: body.important,
     }
+
+    Note.findByIdAndUpdate(req.params.id, note,{ new: true })
+        .then(updatedNote => {
+            res.json(updatedNote);
+        })
+        .catch(err => next(err));
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    notes = notes.filter(note => note.id !== id);
-
-    res.status(204).end();
+    Note.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).end();
+        })
+        .catch(err => next(err));
 });
 
 // Middleware to Handle Unknown Endpoints
 const unknownEndpoint = (req, res) => {
     res.status(404).send({ error: 'Unknown Endpoint '});
 };
-
 app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+    console.log(err.message);
+
+    if (err.name === 'CastError')
+        return res.status(400).send({ error: 'Malformatted ID' });
+
+    next(err);
+}
+app.use(errorHandler);
 
 app.listen(port, () => {
     if (env === "development")
